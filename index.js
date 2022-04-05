ac1ce04f5db410ce061a2891dda1ae42271f8b74
Dylan Jordan <kyle.dylan.jordan@gmail.com>
2022-04-05 10:42:59 -0400

    commit tree c34d4caf9bcf0773c542cf3a94cedeae6233b2be

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+let employee = {
+    name: "Ben",
+    streetAddress: "street"
+}
+
+function updateEmployeeWithKeyAndValue(object, key, value){
+    const copyEmployee = {...object};
+    copyEmployee[key] = value;
+    return copyEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    let copyEmployee = {...employee};
+    delete copyEmployee[key];
+    return copyEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
